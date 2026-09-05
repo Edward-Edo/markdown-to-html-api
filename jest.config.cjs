bfcba8dc29f238300dbcc9806633c59068e37555
Edward Itriago <edwarditriagosub@gmail.com>
2026-09-05 11:15:51 -0400

    fix: configurar Jest para transformar marked-highlight ESM

--- jest.config.cjs
+++ jest.config.cjs
@@ -5,4 +5,8 @@ module.exports = {
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov', 'html'],
   clearMocks: true,
+  transform: {},
+  transformIgnorePatterns: [
+    'node_modules/(?!(marked-highlight|highlight.js)/)',
+  ],
 };
